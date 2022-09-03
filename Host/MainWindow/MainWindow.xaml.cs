7f77e0e1646b35f071bce0ea51a739ccf6c7e15b
Kimpors <mikita_korshak@ukr.net>
2022-09-03 18:39:05 +0300

    Зробив головну сторінку і сторінку налаштувань

---
+++ Host/MainWindow/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Host
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsWindow sw = new SettingsWindow();
+            sw.Show();
+        }
+        private void Stats_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ Host/SettingsWindow/SettingsWindow.xaml.cs
@@ -0,0 +1,18 @@
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Text.RegularExpressions;
+
+namespace Host
+{
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
