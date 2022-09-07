23e40dd4aa22a8987af5088ee893107461e35aa9
kimpors <mikita_korshak@ukr.net>
2022-09-07 17:52:37 +0300

    Добавив клас InputManager для більш простішої перевірки дані, що користувач ввів. Також добавив локальний сервер для всього цього.

--- Client/main.py
+++ Client/main.py
@@ -1,3 +1,27 @@
+from pyrogram import Client
+from pyrogram.errors import FloodWait
+from pyrogram.types import messages_and_media
+import xml.etree.ElementTree as ET
+from deepface import DeepFace
+from time import sleep
+import keyboard
+import sys
 import zmq
 
-print("Hello World")
+apiId = 0
+apiHash = 0
+phone = 0
+
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://127.0.0.1:5555")
+
+socket.send_string("Hello")
+apiId = socket.recv()
+
+print(apiId)
+
+
+
+
+
--- Host/RegisterWindow/RegisterWindow.cs
+++ Host/RegisterWindow/RegisterWindow.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using NetMQ;
+using NetMQ.Sockets;
+using System.Threading;
+using System.IO;
 
 namespace Host
 {
@@ -14,46 +20,155 @@ namespace Host
             InitializeComponent();
         }
 
-        private void TextChangedEventHandler(object sender, TextChangedEventArgs args)
+        public static class InputManager
         {
-            ((TextBox)sender).Background = Brushes.Transparent;
-        } 
+            public static bool isID { get; private set; }
+            public static bool isHASH { get; private set; }
+            public static bool isName { get; private set; }
+            public static bool isPhone { get; private set; }
 
-        private void Check_Click(object sender, RoutedEventArgs e)
+            public static bool isCorrectPhone(string phone)
+            {
+                if (phone.Length > 16 || phone.Length < 10 || phone is null)
+                    return isPhone = false;
+
+                return isPhone = new Regex(@"\+\d{1,3}\d{9,13}").IsMatch(phone);
+            }
+
+            public static bool isCorrectName(string name)
+            {
+                if (name is null || name.Length == 0)
+                    return isName = false;
+
+                return isName = new Regex(@"\w{1,50}").IsMatch(name);
+            }
+
+            public static bool isCorrectID(string id)
+                => isID = id.Length == 8;
+
+            public static bool isCorrectHASH(string hash)
+                => isHASH = hash.Length == 32;
+
+            public static bool isAllCorrect()
+                => true ? isID && isHASH && isName && isPhone : false;
+        }
+
+        private void TextChangedEventHandler(object sender, TextChangedEventArgs args)
+            => ((TextBox)sender).Background = Brushes.Transparent;
+
+        private async void Check_Click(object sender, RoutedEventArgs e)
         {
-            string text = "";
+            ToolTip curr = new ToolTip();
+            curr.IsOpen = true;
             foreach (TextBox tb in CheckTextBox.Children.OfType<StackPanel>().Select(x => ((TextBox)x.Children[1])))
             {
-                if (tb.Text.Length == 0)
-                {
-                    text += "Error" + "\n";
-                    tb.Background = Brushes.DarkRed;
-                    continue;
-                }
-
                 switch (tb.Name)
                 {
-                    case "Phone":
-                        if (new Regex(@"\+\d{1,3}\d{9,13}").IsMatch(tb.Text))
-                            goto default;
-                        tb.Background = Brushes.DarkRed;
-
-                        break;
                     case "BotName":
-                        if (new Regex(@"\w{1,50}").IsMatch(tb.Text))
+                        if (!InputManager.isCorrectName(tb.Text))
+                        {
+                            tb.ToolTip = new ToolTip()
+                            {
+                                Content = "Please enter right name(max 50 symbols(not numbers))",
+                                IsOpen = true
+                            };
                             goto default;
-                        tb.Background = Brushes.DarkRed;
+                        }
+                        break;
 
+                    case "Phone":
+                        if (!InputManager.isCorrectPhone(tb.Text))
+                        {
 
+                            tb.ToolTip = new ToolTip()
+                            {
+                                Content = "Please enter right phone number{+{country code}{numbers}}",
+                                IsOpen = true,
+                            };
+                            goto default;
+                        }
+                        break;
+
+                    case "Hash":
+                        if (!InputManager.isCorrectHASH(tb.Text))
+                        {
+                            tb.ToolTip = new ToolTip()
+                            {
+                                Content = "Please enter right hash(32 symbold)",
+                                IsOpen = true
+                            };
+                            goto default;
+                        }
+                        break;
+
+                    case "ID":
+                        if (!InputManager.isCorrectID(tb.Text))
+                        {
+                            tb.ToolTip = new ToolTip()
+                            {
+                                Content = "Please enter right id(8 numbers)",
+                                IsOpen = true
+                            };
+                            goto default;
+                        }
                         break;
                     default:
-                        text += tb.Text + "\n";
+                        tb.Background = new SolidColorBrush(Color.FromArgb(255, 160, 25, 20));
                         break;
                 }
             }
 
-            MessageBox.Show(text);
-        }
+            if (!InputManager.isAllCorrect())
+            {
+                string script = @$"{new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.FullName}\Client\main.py", fileName = "";
+                MessageBox.Show(script);
 
+                await Task.Run(() =>
+                {
+                    ProcessStartInfo processInfo = new ProcessStartInfo("cmd.exe")
+                    {
+                        CreateNoWindow = false,
+                        RedirectStandardOutput = true,
+                        RedirectStandardInput = true,
+                        UseShellExecute = false
+                    };
+
+                    using (Process getPythonExePath = Process.Start(processInfo))
+                    {
+                        getPythonExePath.StandardInput.WriteLine(@"py -c ""import sys;print(sys.executable)""");
+                        getPythonExePath.StandardInput.Flush();
+                        getPythonExePath.StandardInput.Close();
+                        getPythonExePath.WaitForExit();
+                        fileName = getPythonExePath.StandardOutput.ReadToEnd().Split('\n')[4];
+                    }
+
+                    processInfo.FileName = fileName;
+                    processInfo.Arguments = @$"""{script}""";
+
+                    using (var server = new ResponseSocket())
+                    {
+                        server.Bind("tcp://*:5555");
+
+                        using (Process client = Process.Start(processInfo))
+                        {
+
+                            string msg;
+                            while (true)
+                            {
+                                msg = server.ReceiveFrameString();
+                                MessageBox.Show(msg + " message");
+
+                                switch (msg)
+                                {
+                                    case "Hello":
+                                        server.SendFrame("World");
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+        }
     }
 }
